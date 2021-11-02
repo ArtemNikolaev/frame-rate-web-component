bb56a238a2833873daccf006a020e6257bee144a
Artem Nikolaev <artem@nikolaev.by>
2021-11-02 12:54:09 +0300

    move on/off func in special file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 - readme for project
 - on/off functionality
+- on/off demo
 ### Changed
 - line separators
 
--- src/index.js
+++ src/index.js
@@ -1,52 +1,24 @@
 import styleEl from './style/style';
 import generatePostLikeTheme from './themes/post-like';
+import FrameRateCount from "./utils/frame-rate-count";
 
 class FrameRate extends HTMLElement {
   constructor() {
     super();
-    this.isInactive = false;
-
-    this.frameAmount = [];
-    this.frameRate = 0;
-    this.frameRateOld = 0;
 
     this.attachShadow({mode: 'open'});
     this.shadowRoot.appendChild(styleEl);
 
-    this.theme = generatePostLikeTheme(this.frameRate);
+    this.theme = {
+      style : document.createElement('style'),
+      el: document.createElement('div'),
+    };
 
-    this.render();
-  }
-
-  connectedCallback() {
-    this.on();
-  }
-
-  counter() {
-    if (this.isInactive) {
-      this.frameAmount.splice(0);
-
-      return;
-    }
-
-    this.frameAmount = this.frameAmount.filter( dt => dt > Date.now() - 1000);
-    this.frameAmount.push(Date.now());
-
-    const frameRate = this.frameAmount.length;
-
-    if (frameRate !== this.frameRate) {
-      this.frameRateOld = this.frameRate;
-      this.frameRate = frameRate;
-
-      this.render();
-    }
-
-
-    requestAnimationFrame(() => this.counter());
+    this.frCounter = new FrameRateCount(this.render.bind(this));
   }
 
   disconnectedCallback() {
-    this.isInactive = true;
+    this.frCounter.off();
   }
 
   attributeChangedCallback(key, prev, next) {
@@ -57,24 +29,18 @@ class FrameRate extends HTMLElement {
     } catch (e) { }
   }
 
-  render() {
+  render(frameRate) {
     this.theme.style.remove();
     this.theme.el.remove();
 
-    this.theme = generatePostLikeTheme(this.frameRate);
+    this.theme = generatePostLikeTheme(frameRate);
 
     this.shadowRoot.appendChild(this.theme.style);
     this.shadowRoot.appendChild(this.theme.el);
   }
 
-  on() {
-    this.counter();
-  }
-
   off(prev, next) {
-    this.isInactive = typeof next === 'string';
-
-    if (!this.isInactive) this.counter();
+    typeof next === 'string' ? this.frCounter.off() : this.frCounter.on();
   }
 
   static get observedAttributes() {return ['off'];}
